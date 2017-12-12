8d81ba402352248d6da1b71aa3a310ac8f0e5f37
Evangeline Ireland <eireland@concord.org>
2017-12-12 14:12:44 -0800

    Added date filter in the form so can specify query date. Default date is still today's date if end date is not provided

--- index.js
+++ index.js
@@ -31,6 +31,39 @@ app.listen(app.get('port'), function () {
 
 app.post('/db', function (request, response) {
 
+  function parseJSON(message_str){
+    var join_string='',
+        key='' ,
+        list='',
+        list_arr=[];
+    var message;
+    console.log("in parseQuery. Message is: "+message_str);
+    //join_sql =  "INNER JOIN ( "
+    //join_sql << "   VALUES "
+    //join_sql << "       ('" << clean_item_list.join("'), ('") << "') "
+    //join_sql << "   ) vals(v) ON "
+    //join_sql << "( #{hstore_columns} -> '#{clean_key}' ) = v"
+
+
+    //SELECT time, application, session, username, activity, event, event_value, parameters FROM logs INNER JOIN (VALUES("https://learn.concord.org/dataservice/external_activity_data/19507da6-8c48-4de9-8a02-0686248b396d"),("https://learn.concord.org/dataservice/external_activity_data/b7894aad-9529-49a3-b530-81c3108f08d4"),("https://learn.concord.org/dataservice/external_activity_data/86248b91-b047-4e12-bdec-6cb9190f553b")) vals(v) ON (parameter || extras->'run_remote_endpoint') = v WHERE application='GeniStarDev' and time<'2017-12-15' order by time desc limit 1
+
+    message = JSON.parse(message_str);
+    console.log("filter is: "+ message.filter[0]);
+
+    key = message.filter[0].key;
+    list_arr = message.filter[0].list;
+    console.log("key is: " +key);
+
+    for (var i=0;i<list_arr.length;i++){
+      list = list + "('"+list_arr[i]+"'),";
+    }
+    list = list.slice(0,-1);
+    console.log("list is: "+list);
+
+    join_string = "INNER JOIN (VALUES" + list + ") vals(v) ON (parameters || extras->'" + key + "') = v ";
+    return join_string;
+  }
+
   const pool = new Pool({
     connectionString: connectionString,
   });
@@ -41,6 +74,10 @@ app.post('/db', function (request, response) {
   console.log(query_end_date);
   var query_start_date = null;
   var activity_name = null;
+  var event_name = null;
+  var session_id = null;
+  var json_query = null;
+  var temp_table_join = null;
 
   var application = request.body.application;
   var num_records = 100; //default num records if user does not enter a num records
@@ -61,18 +98,44 @@ app.post('/db', function (request, response) {
     activity_name = request.body.activity_name;
   }
 
-  var query_string = "SELECT time, application, session, username, activity, event, event_value, parameters FROM logs WHERE application='" + application + "'";
+  if (request.body.event_name != '' && request.body.event_name != null) {
+    event_name = request.body.event_name;
+  }
 
+  if (request.body.session_id != '' && request.body.session_id != null) {
+    session_id = request.body.session_id;
+  }
+
+  if (request.body.json_query != '' && request.body.json_query != null) {
+    json_query = request.body.json_query;
+    temp_table_join = parseJSON(json_query);
+  }
+
+
+  var query_string = "SELECT time, application, session, username, activity, event, event_value, parameters FROM logs ";
+
+  if (json_query !=null) {
+    query_string = query_string + temp_table_join + "WHERE application='" + application + "'";
+  }
+  else {
+    query_string = query_string + "WHERE application='" + application + "'";
+  }
 // select distinct activity from logs where application not like 'Unknown%' and activity like '%Untitled%' order by activity desc;
   if (activity_name != null) {
     query_string = query_string + " and activity like '%" + activity_name +"%'"
   }
+  if (event_name != null) {
+    query_string = query_string + " and event like '%" + event_name +"%'"
+  }
+  if (session_id != null) {
+    query_string = query_string + " and session like '%" + session_id +"%'"
+  }
+  
   if (query_start_date!= null){
     query_string = query_string + " and time>'" + query_start_date + "'";
   }
 
   query_string = query_string  + " and time<'" + query_end_date + "' order by time desc limit " + num_records;
-
   console.log("Query string is: " + query_string);
 
   pool.query(query_string, function (err, result) {
